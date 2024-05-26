ac8af7bb40b0d8604084c2814ff32c05e9d61da4
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-26 18:18:32 +0200

    Start to add the addBookToLibrary function
    The function can't work for the moment, the Book constructor don't exist

--- script.js
+++ script.js
@@ -10,6 +10,16 @@ openform.addEventListener("click", () => {
 
 submit.addEventListener("click", (event) => {
     event.preventDefault();
+    if (read.checked) {
+        read.value = "read";
+    } else {
+        read.value = "not read";
+    }
+    addBookToLibrary(title.value, author.value, pages.value, read.value);
     dialog.close();
 
 })
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
+}
