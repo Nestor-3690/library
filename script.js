bc9ca2d43216e61334e443f8c7e77ea4ddeefc74
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-26 09:15:51 +0200

    Add event listener to open the dialog

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ const openform = document.querySelector("#openform");
 const dialog = document.querySelector("#dialog");
 const submit = document.querySelector("#submit");
 
-
 openform.addEventListener("click", () => {
     dialog.showModal();
-})
-
-submit.addEventListener("click", (event) => {
-    event.preventDefault();
-    dialog.close();
-
 })
