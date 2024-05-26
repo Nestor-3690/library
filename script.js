c5321543b9277b15301c83a9563ebd4bb13b0613
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-26 09:15:12 +0200

    Add variables for buttons and dialog

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const myLibrary = [];
+
+const openform = document.querySelector("#openform");
+const dialog = document.querySelector("#dialog");
+const submit = document.querySelector("#submit");
+
+
+openform.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+submit.addEventListener("click", (event) => {
+    event.preventDefault();
+    dialog.close();
+
+})
