a3bf97655b80f852e5f6df11a5d2f4bf6733d29e
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-27 09:29:59 +0200

    Add Book constructor

--- script.js
+++ script.js
@@ -20,6 +20,13 @@ submit.addEventListener("click", (event) => {
 
 })
 
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
