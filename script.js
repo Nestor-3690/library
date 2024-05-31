637bdae4aaf5093d3817eb003d436cb69418eb24
Nestor-3690 <160883075+Nestor-3690@users.noreply.github.com>
2024-05-31 16:17:09 +0200

    Reorganize the script into two factories
    The DisplayController one is immediately called
    The Library one is called inside DisplayController

--- script.js
+++ script.js
@@ -1,93 +1,105 @@
-const myLibrary = [];
+function Library() {
+    const myLibrary = [];
 
-const infos = ["title", "author", "pages", "read"];
+    const getLibrary = () => myLibrary;
 
-const openform = document.querySelector("#openform");
-const dialog = document.querySelector("#dialog");
-const submit = document.querySelector("#submit");
-
-const container = document.querySelector(".container");
-
-openform.addEventListener("click", () => {
-    dialog.showModal();
-})
-
-submit.addEventListener("click", (event) => {
-    event.preventDefault();
-    if (read.checked) {
-        read.value = "read";
-    } else {
-        read.value = "not read";
-    }
-    addBookToLibrary(title.value, author.value, pages.value, read.value);
-    displayBooks();
-    dialog.close();
-
-})
-
-class Book {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-    changeReadStatus = () => {
-        if (this.read === "read") {
-            this.read = "not read";
-        } else {
-            this.read = "read";
+    class Book {
+        constructor(title, author, pages, read) {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.read = read;
         }
-    }
-}
-
-function addBookToLibrary(title, author, pages, read) {
-    const book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-}
-
-function displayBooks() {
-    container.textContent = '';
-    let count = 0;
-    myLibrary.forEach((book) => {
-        const bookdiv = document.createElement("div");
-        bookdiv.setAttribute("class", "book");
-        bookdiv.setAttribute("id", count);
-        for (info in book) {
-            if (infos.includes(info)) {
-                const book_info = document.createElement("div");
-                if (info === "title") {
-                    book_info.setAttribute("class", "title");
-                } else if (info === "author") {
-                    book_info.setAttribute("class", "author");
-                } else if (info === "pages") {
-                    book_info.setAttribute("class", "pages");
-                } else if (info == "read") {
-                    book_info.setAttribute("class", "read");
-                }
-                book_info.textContent = book[info];
-                bookdiv.append(book_info);
+        changeReadStatus = () => {
+            if (this.read === "read") {
+                this.read = "not read";
+            } else {
+                this.read = "read";
             }
         }
-        const cancel_button = document.createElement("button");
-        cancel_button.setAttribute("class", "cancel");
-        cancel_button.textContent = "Remove";
-        cancel_button.addEventListener("click", () => {
-            myLibrary.splice(bookdiv.id, 1);
-            displayBooks();
-        })
+    }
 
-        const read_status = document.createElement("button");
-        read_status.setAttribute("class", "read-status");
-        read_status.textContent = "Change Read Status";
-        read_status.addEventListener("click",() => {
-            myLibrary[bookdiv.id].changeReadStatus();
-            displayBooks();
-        })
+    function addBookToLibrary(title, author, pages, read) {
+        const book = new Book(title, author, pages, read);
+        myLibrary.push(book);
+    }
 
-        bookdiv.append(cancel_button);
-        bookdiv.append(read_status);
-        container.append(bookdiv);
-        count++;
-    })
+    return { getLibrary, addBookToLibrary }
+
 }
+
+(function DisplayController() {
+    const library = Library();
+
+    const myLibrary = library.getLibrary()
+
+    const openform = document.querySelector("#openform");
+    const dialog = document.querySelector("#dialog");
+    const submit = document.querySelector("#submit");
+    const container = document.querySelector(".container");
+
+    openform.addEventListener("click", () => {
+        dialog.showModal();
+    })
+
+    submit.addEventListener("click", (event) => {
+        event.preventDefault();
+        if (read.checked) {
+            read.value = "read";
+        } else {
+            read.value = "not read";
+        }
+        library.addBookToLibrary(title.value, author.value, pages.value, read.value);
+        displayBooks();
+        dialog.close();
+    
+    })
+
+    function displayBooks() {
+        container.textContent = '';
+        let count = 0;
+        const infos = ["title", "author", "pages", "read"];
+        myLibrary.forEach((book) => {
+            const bookdiv = document.createElement("div");
+            bookdiv.setAttribute("class", "book");
+            bookdiv.setAttribute("id", count);
+            for (info in book) {
+                if (infos.includes(info)) {
+                    const book_info = document.createElement("div");
+                    if (info === "title") {
+                        book_info.setAttribute("class", "title");
+                    } else if (info === "author") {
+                        book_info.setAttribute("class", "author");
+                    } else if (info === "pages") {
+                        book_info.setAttribute("class", "pages");
+                    } else if (info == "read") {
+                        book_info.setAttribute("class", "read");
+                    }
+                    book_info.textContent = book[info];
+                    bookdiv.append(book_info);
+                }
+            }
+            const cancel_button = document.createElement("button");
+            cancel_button.setAttribute("class", "cancel");
+            cancel_button.textContent = "Remove";
+            cancel_button.addEventListener("click", () => {
+                myLibrary.splice(bookdiv.id, 1);
+                displayBooks();
+            })
+    
+            const read_status = document.createElement("button");
+            read_status.setAttribute("class", "read-status");
+            read_status.textContent = "Change Read Status";
+            read_status.addEventListener("click",() => {
+                myLibrary[bookdiv.id].changeReadStatus();
+                displayBooks();
+            })
+    
+            bookdiv.append(cancel_button);
+            bookdiv.append(read_status);
+            container.append(bookdiv);
+            count++;
+        })
+    }
+
+})();
